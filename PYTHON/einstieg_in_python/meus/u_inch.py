6627fa38f686505e490d6322a069842ff59e119a
rao003 <49493736+rao003@users.noreply.github.com>
2019-11-30 09:39:50 +0100

    py

---
+++ PYTHON/einstieg_in_python/meus/u_inch.py
@@ -0,0 +1,6 @@
+print ("type a number to conver from inches to cm")
+i = int(input())
+c = 2.54*i
+print(c)
+
+
---
+++ PYTHON/einstieg_in_python/meus/u_steuer.py
@@ -0,0 +1,5 @@
+print ("type your salary")
+salary = int(input())
+taxes = 0.18
+result = salary * taxes
+print("from your salary, the taxes are", result)
