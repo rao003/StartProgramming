e7e51a911b12758169a4bb9b8e3f169ddd47c4eb
rao003 <49493736+rao003@users.noreply.github.com>
2020-03-09 11:03:09 +0100

    mathe

---
+++ PYTHON/mathe_score.py
@@ -0,0 +1,19 @@
+import random
+import time
+import math
+a = 0
+b = 0
+c = 0
+score = 0
+for x in range(10):
+	a = random.randint(0,10)
+	b = random.randint(0,10)
+	print(a)
+	print("x")
+	print(b)
+	c = int(input("What is the result?"))
+	if c == a * b:
+		score = score + 1
+print("From 10 you got")
+print(score)
+print("correct")
